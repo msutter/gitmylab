38f15794fef6f3f0fc5640407b17bc32ab5d3e21
Marc Sutter <marc.sutter@swisscom.com>
2015-02-02 11:40:29 +0100

    remove old code

--- lib/gitmylab/commands/access.rb
+++ lib/gitmylab/commands/access.rb
@@ -89,69 +89,6 @@ module Gitmylab
         end
       end
 
-
-      def access_old_iterator(cli_options, enumerable)
-        cli_iterator enumerable do |item|
-          sr         = Cli::Result.new(item)
-          sr.command = @command
-          sr.action  = @action
-          sr.message = ''
-          if cli_options['users']
-            cli_options['users'].each do |username|
-              user = Gitmylab::Gitlab::User.find_by_username(username).first
-              if user
-                permission = Gitmylab::Access::Permission.new(user, item, cli_options['level'])
-                case @action
-                when :list
-                  if permission.list
-                    sr.message << "#{(user.username + ' / ' + user.name).ljust(LeftAdjust)} => #{' '*RightAdjust + access_level}\n"
-                    sr.status  = :success
-                  else
-                    sr.message << "#{(user.username + '/' + user.name).ljust(LeftAdjust)} => #{' '*RightAdjust}No direct access to #{item.path}\n"
-                    sr.status  = :skip
-                  end
-                when :add
-                  options = {:regression => cli_options['regression']}
-                  r = permission.create(options)
-                  sr.status = r.status
-                  sr.message << case r.reason
-                  when :exists then "#{user.username} already has #{r.access} access !\n"
-                  when :regression then "#{user.username} already has #{r.access} access !, Use -R to force regression\n"
-                  else "Access Level #{cli_options['level']} set for #{user.username}\n"
-                  end
-                when :remove
-                  access_level = permission.list
-                  if permission.list
-                    r = permission.remove
-                    sr.status = :success
-                    sr.message << "Access #{access_level} removed for #{user.username}.\n"
-                  else
-                    sr.message << "User #{user.username} already has no access.\n"
-                    sr.status = :skip
-                  end
-                end
-              else
-                sr.status  = :skip
-                sr.message = "User #{username} not found !\n"
-              end
-            end
-          else
-            # only for action list without defined users
-            users_selection = item.members
-            sr.message << "No members found for #{item.path}\n" if users_selection.empty?
-            sr.status  = :empty
-
-            users_selection.sort_by{|member| member.access_level }.reverse.each do |member|
-              access_name = Gitmylab::Access::Base.access_name(member.access_level)
-              sr.message << "#{(member.username + ' / ' + member.name).ljust(LeftAdjust)} => #{' '*RightAdjust + access_name.to_s}\n"
-              sr.status  = :success
-            end
-          end
-          sr.render
-
-        end
-      end
-
       def get_items_count(sp, sg)
         ic = spc + sgc
         if ic == 0
